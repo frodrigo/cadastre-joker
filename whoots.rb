327d3fe6e613090a6ca412baedee7e23129d2c49
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2015-02-19 21:10:35 +0100

    Change index page

--- whoots.rb
+++ whoots.rb
@@ -79,59 +79,38 @@ end
 __END__
 
 @@ layout
-<html><head><title>WhooTS - the tiny public wms to tms proxy</title>
-<style>body{font-family: Arial, Helvetica, sans-serif;}</style>
-</head><body>
-<h1><img src="whoots_tiles.jpg" />
-WhooTS - the tiny public wms to tms proxy</h1>
 <%= yield %>
-<hr />
-<p>About: Made with Sinatra and Ruby by Tim Waters tim@geothings.net  <a href="http://thinkwhere.wordpress.com/">Blog</a><br />
-Code available at: <a href="http://github.com/timwaters/whoots">github</a></p>
-</body></html>
 
 @@ index
-<h2>What is it?</h2>
-<p>It's a simple WMS to Google/OSM Scheme TMS proxy. You can use WMS servers in applications which only use those pesky "Slippy Tiles"
-</p>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Redirection HTTP de tuile TMS vers le WMS du Cadastre</title>
+    <meta charset="utf-8" />
 
+    <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+</head>
+<body>
+    <div id="map" style="width: 100%; height: 600px"></div>
 
-<h2>Usage:</h2>
-<h4>http://<%=request.host%>:<%=request.port%>/tms/z/x/y/{layer}/http://path.to.wms.server</h4>
+    <script>
+        var map = L.map('map').setView([44.8265, -0.5692], 13);
 
-e.g<br />
-http://<%=request.host%>:<%=request.port%>/tms/!/!/!/2013/http://warper.geothings.net/maps/wms/2013<br />
-http://<%=request.host%>:<%=request.port%>/tms/z/x/y/870/http://maps.nypl.org/warper/layers/wms/870<br />
-<br />
-Using this WMS server:<br />
-http://hypercube.telascience.org/cgi-bin/mapserv?map=/home/ortelius/haiti/haiti.map&request=getMap&service=wms&version=1.1.1&format=image/jpeg&srs=epsg:4326&exceptions=application/vnd.ogc.se_inimage&layers=HAITI&<br /><br />
-http://<%=request.host%>:<%=request.port%>/tms/!/!/!/HAITI/http://hypercube.telascience.org/cgi-bin/mapserv?map=/home/ortelius/haiti/haiti.map<br />
-<br />
-http://<%=request.host%>:<%=request.port%>/tms/19/154563/197076/870/http://maps.nypl.org/warper/layers/wms/870
-
-<h2>Openstreetmap Potlatch editing example </h2>
-
-<h3>Map Warper <a href="http://warper.geothings.net">http://warper.geothings.net</a> </h3>
-WMS Link: http://warper.geothings.net/maps/wms/2013<br />
-<br />
-http://www.openstreetmap.org/edit?lat=18.601316&lon=-72.32806&zoom=18&tileurl=http://<%=request.host%>:<%=request.port%>/tms/!/!/!/2013/http://warper.geothings.net/maps/wms/2013
-
-<h3>NYPL Map Rectifier <a href="http://maps.nypl.org">http://maps.nypl.org</a></h3>
-
-http://www.openstreetmap.org/edit?lat=40.73658&lon=-73.87108&zoom=17&tileurl=http://<%=request.host%>:<%=request.port%>/tms/!/!/!/870/http://maps.nypl.org/warper/layers/wms/870
-
-
-
-<h3>Telascience Haiti <a href="http://hypercube.telascience.org/haiti/">http://hypercube.telascience.org/haiti/</a></h3>
-http://www.openstreetmap.org/edit?lat=18.601316&lon=-72.32806&zoom=18&tileurl=http://<%=request.host%>:<%=request.port%>/tms/!/!/!/HAITI/http://hypercube.telascience.org/cgi-bin/mapserv?map=/home/ortelius/haiti/haiti.map<br /><br />
-
-<h2>Example Outputs</h2>
-http://hypercube.telascience.org/cgi-bin/mapserv?bbox=-8051417.93739076,2107827.49199202,-8051265.06333419,2107980.36604859&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:900913&width=256&height=256&layers=HAITI&map=/home/ortelius/haiti/haiti.map&styles=
-<br /><br />
-http://maps.nypl.org/warper/layers/wms/870?bbox=-8223095.50291926,4973298.80834671,-8222789.75480612,4973604.55645985&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:900913&width=256&height=256&layers=870&map=&styles=
-<br />
-
-<h2>Important Notes</h2>
-* Use only a WMS that supports SRS EPSG:900913 <br />
-* Tiles are Google / OSM Scheme
-
+        L.tileLayer('/*/tout/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: 'Cadastre',
+        }).addTo(map);
+        L.tileLayer('/**/tout/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: 'Cadastre',
+        }).addTo(map);
+    </script>
+<% host = request.host + (request.port != 80 ? ":#{request.port.to_s}" : "") %>
+tms[20]:http://<%= host %>/{insee}/{style}/{z}/{x}/{y}.png
+<ul>
+  <li>{insee}: à remplacer par le code insee de la commune ou par "*" pour le mode joker automatique (et double joker "**" pour les tuiles complémentaires à cheval sur plusieurs communes)</li>
+  <li>{style}: "tout", "semi", "transp" ou un style personalisé du cadastre</li>
+</lu>
+</body>
+</html>
