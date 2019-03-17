1b442a2a4bb21ddbfb2c1ad86cf8696e95d5d5bd
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-03-17 23:09:03 +0100

    Ajout de SetSRID() - TODO par jocelyn: à vérifier

--- whoots.rb
+++ whoots.rb
@@ -46,9 +46,9 @@ get '/:insee/:style/:z/:x/:y.png' do
     con_pool.with{ |conn|
       conn.exec( """
 SELECT insee
-FROM \"communes-20150101-5m\"
-WHERE the_geom && ST_MakeLine(ST_MakePoint(#{min[:lng_deg]}, #{min[:lat_deg]}), ST_MakePoint(#{max[:lng_deg]}, #{max[:lat_deg]}))
-ORDER BY ST_Area(ST_Intersection(the_geom, ST_Envelope(ST_MakeLine(ST_MakePoint(#{min[:lng_deg]}, #{min[:lat_deg]}), ST_MakePoint(#{max[:lng_deg]}, #{max[:lat_deg]}))))) DESC, insee
+FROM \"communes\"
+WHERE the_geom && ST_SetSRID(ST_MakeLine(ST_MakePoint(#{min[:lng_deg]}, #{min[:lat_deg]}), ST_MakePoint(#{max[:lng_deg]}, #{max[:lat_deg]})),4326)
+ORDER BY ST_Area(ST_Intersection(the_geom, ST_SetSRID(ST_Envelope(ST_MakeLine(ST_MakePoint(#{min[:lng_deg]}, #{min[:lat_deg]}), ST_MakePoint(#{max[:lng_deg]}, #{max[:lat_deg]}))),4326))) DESC, insee
 LIMIT 1 OFFSET #{offset}""" ) do |result|
         result.each do |row|
           insee = row.values_at('insee')[0]
